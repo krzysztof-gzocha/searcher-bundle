d7a1145e8cf74b9908c72b2bc2035d65562720ad
Krzysztof Gzocha <kgzocha@gmail.com>
2016-01-21 20:49:42 +0400

    Init

---
+++ src/KGzocha/Bundle/SearcherBundle/KGzochaSearcherBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace KGzocha\Bundle\SearcherBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Krzysztof Gzocha <krzysztof@propertyfinder.ae>
+ * @package KGzocha\Bundle\SearcherBundle
+ */
+class KGzochaSearcherBundle extends Bundle
+{
+}
