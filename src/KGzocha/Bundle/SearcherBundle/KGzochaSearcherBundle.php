eb46159f23119d41f7444d10894fddbcc39d401c
Krzysztof Gzocha <kgzocha@gmail.com>
2016-01-31 14:19:55 +0400

    SearcherFactoryCompilerPassTest

--- src/KGzocha/Bundle/SearcherBundle/KGzochaSearcherBundle.php
+++ src/KGzocha/Bundle/SearcherBundle/KGzochaSearcherBundle.php
@@ -3,6 +3,7 @@
 namespace KGzocha\Bundle\SearcherBundle;
 
 use KGzocha\Bundle\SearcherBundle\DependencyInjection\CompilerPass\FilterImposerCollection;
+use KGzocha\Bundle\SearcherBundle\DependencyInjection\CompilerPass\SearcherFactoryCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use KGzocha\Bundle\SearcherBundle\DependencyInjection\CompilerPass\NamedFilterModelCollection;
@@ -18,6 +19,8 @@ class KGzochaSearcherBundle extends Bundle
 
     const NAMED_COLLECTION_TAG = 'searcher.named_filter_model_collection';
     const NAMED_MODEL_TAG = 'searcher.named_model';
+
+    const SEARCHER_FACTORY_TAG = 'searcher.factory';
 
     const CONTEXT_ID = 'contextId';
 
@@ -44,6 +47,11 @@ class KGzochaSearcherBundle extends Bundle
             ->addCompilerPass(new FilterImposerCollection(
                 self::IMPOSER_COLLECTION_TAG,
                 self::IMPOSER_TAG,
+                self::CONTEXT_ID
+            ))
+            ->addCompilerPass(new SearcherFactoryCompilerPass(
+                self::SEARCHER_FACTORY_TAG,
+                self::IMPOSER_COLLECTION_TAG,
                 self::CONTEXT_ID
             ));
     }
---
+++ src/KGzocha/Bundle/SearcherBundle/Test/DependencyInjection/CompilerPass/SearcherFactoryCompilerPassTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace KGzocha\Bundle\SearcherBundle\Test\DependencyInjection\CompilerPass;
+
+use KGzocha\Bundle\SearcherBundle\DependencyInjection\CompilerPass\SearcherFactoryCompilerPass;
+use KGzocha\Bundle\SearcherBundle\DependencyInjection\SearcherBundleExtension;
+use KGzocha\Searcher\FilterImposer\Collection\FilterImposerCollection;
+use KGzocha\Searcher\Searcher\Searcher;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
+/**
+ * @author Krzysztof Gzocha <krzysztof@propertyfinder.ae>
+ * @package KGzocha\Bundle\SearcherBundle\Test\DependencyInjection\CompilerPass
+ */
+class SearcherFactoryCompilerPassTest extends \PHPUnit_Framework_TestCase
+{
+    const FACTORY_TAG = 'factoryTag';
+    const IMPOSER_COLLECTION_TAG = 'imposerCollectionTag';
+    const CONTEXT_ID_NAME = 'contextIdName';
+
+    public function testProcess()
+    {
+        $container = $this->prebuildContainer();
+
+        $searcherDefinition = new Definition();
+        $searcherDefinition->addTag(self::FACTORY_TAG, [
+            self::CONTEXT_ID_NAME => 'someSearch',
+        ]);
+
+        $imposerCollection = new Definition();
+        $imposerCollection
+            ->setClass(FilterImposerCollection::class)
+            ->addTag(self::IMPOSER_COLLECTION_TAG, [
+                self::CONTEXT_ID_NAME => 'someSearch',
+            ]);
+
+        $container
+            ->addDefinitions([
+                'searcher_service_name' => $searcherDefinition,
+                'some_imposers' => $imposerCollection
+            ]);
+
+        $this->process($container);
+        $this->assertInstanceOf(
+            Searcher::class,
+            $container->get('searcher_service_name')
+        );
+    }
+
+    /**
+     * @return ContainerBuilder
+     */
+    private function prebuildContainer()
+    {
+        $container = new ContainerBuilder();
+
+        $dispatcher = new Definition(EventDispatcher::class);
+        $container->addDefinitions([
+            'event_dispatcher' => $dispatcher,
+        ]);
+
+        $extension = new SearcherBundleExtension();
+        $container->registerExtension($extension);
+        $container->loadFromExtension('searcher_bundle');
+        $extension->load([], $container);
+
+        return $container;
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     */
+    private function process(ContainerBuilder $container)
+    {
+        $pass = new SearcherFactoryCompilerPass(
+            self::FACTORY_TAG,
+            self::IMPOSER_COLLECTION_TAG,
+            self::CONTEXT_ID_NAME
+        );
+
+        $pass->process($container);
+    }
+}
